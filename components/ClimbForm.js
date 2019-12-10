65f2c8e300d86098999e7d4910efb01ce9b6f78f
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2019-12-09 22:54:02 -0800

    date; time; total; highest difficulty

--- components/ClimbForm.js
+++ components/ClimbForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 import ClimbButton from './ClimbButton';
 
-export default function ClimbForm({}) {
+export default function ClimbForm({ date, total, time, highestDiff }) {
 	return (
 		<View style={styles.formContainer}>
-			<ClimbButton color='blue' title='CLIMB BUTTON!' />
+			<View style={styles.topRow}>
+				<Text>Date: {date}</Text>
+				<Text>Total Climbs: {total}</Text>
+			</View>
+			<View style={styles.secondRow}>
+				<Text>Time: {time}</Text>
+				<Text>Highest Difficulty: {highestDiff}</Text>
+			</View>
+			<ClimbButton color='green' title='Edit Climb' />
 		</View>
 	);
 }
@@ -20,5 +28,15 @@ const styles = StyleSheet.create({
 		padding: 15,
 		margin: 15,
 		marginBottom: 0
+	},
+	topRow: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	secondRow: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
 	}
 });
--- components/ClimbList.js
+++ components/ClimbList.js
@@ -8,8 +8,18 @@ export default function ClimbList({}) {
 	return (
 		<View>
 			<StartingClimbToggle />
-			<ClimbForm />
-			<ClimbForm />
+			<ClimbForm
+				date='10/09/19'
+				total={20}
+				highestDiff='V5'
+				time='1h14m50s'
+			/>
+			<ClimbForm
+				date='12/12/19'
+				total={10}
+				highestDiff='V6'
+				time='2h01m18s'
+			/>
 		</View>
 	);
 }
