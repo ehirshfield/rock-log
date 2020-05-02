a63c0c8ccc5143a8720fd5cb3e65ef4d73c644f2
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2020-05-02 13:12:07 -0700

    move convertTime func to common file

--- components/ClimbTimer.js
+++ components/ClimbTimer.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { colors } from '../theme';
+import { convertTime } from '../utils/common';
 
 export default class ClimbTimer extends React.Component {
 	render() {
-		const convertTime = (time) => {
-			let milliseconds = parseInt((time % 1000) / 100);
-			let seconds = Math.floor((time / 1000) % 60);
-			let minutes = Math.floor((time / (1000 * 60)) % 60);
-			let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-
-			hours = hours < 10 ? '0' + hours : hours;
-			minutes = minutes < 10 ? '0' + minutes : minutes;
-			seconds = seconds < 10 ? '0' + seconds : seconds;
-
-			return hours + ':' + minutes + ':' + seconds + milliseconds;
-		};
-
 		return (
 			<View style={styles.timerContainer}>
 				<Text style={styles.timerText}>
@@ -32,9 +20,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		flexDirection: 'row',
 	},
-	hourContainer: {},
-	minuteContainer: {},
-	secondContainer: {},
 	timerText: {
 		color: colors.textColors.generalText,
 		fontSize: 40,
--- utils/common.js
+++ utils/common.js
@@ -4,3 +4,16 @@ export function calculateHighestDifficulty(climbs) {
 	});
 	return Math.max(...numbers);
 }
+
+export function convertTime(time) {
+	let milliseconds = parseInt((time % 1000) / 100);
+	let seconds = Math.floor((time / 1000) % 60);
+	let minutes = Math.floor((time / (1000 * 60)) % 60);
+	let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+
+	return hours + ':' + minutes + ':' + seconds + milliseconds;
+}
